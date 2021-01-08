aaadb3195ea5517ac604777668aa3cdc9f16828b
winter_wang <wd18510488302@163.com>
2021-01-08 21:37:58 +0800

    author:wangdong date:20210108 message:share-plus

---
+++ wd-website/src/main/java/com/wd/share/plus/AppWebsite.java
@@ -0,0 +1,11 @@
+package com.wd.share.plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebsite {
+    public static void main(String[] args){
+        SpringApplication.run(AppWebsite.class,args);
+    }
+}
---
+++ wd-website/src/main/resources/static/js/login.js
@@ -0,0 +1,20 @@
+layui.use(['layer','form'], function(){
+    var form = layui.form;
+    var layer = layui.layer;
+
+    //监听提交
+    form.on('submit(formDemo)', function (data) {
+        var action = data.form.action;//表单提交URL地址
+        console.log(JSON.stringify(data.field));//表单数据
+        $.post(action, data.field, function (obj) {
+        	if(obj.completeCode == 0){
+        		alert(obj.reasonMessage);
+        		location.href="./login.jsp";
+        	}else{
+        		//根据返回结果作出相应处理
+        		location.href="./index?username="+obj.data.username;
+        	}
+        })
+        return false;//注释掉这行代码后，表单将会以普通方式提交表单，否则以ajax方式提交表单
+    });
+});
